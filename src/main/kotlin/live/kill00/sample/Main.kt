ce642a4efadabcddf443e47bace0a4b36edb7a36
Kill00 <kill00@p.teamvery.dev>
2023-12-26 21:25:10 +0900

    버그수정

--- src/main/kotlin/live/kill00/sample/Main.kt
+++ src/main/kotlin/live/kill00/sample/Main.kt
@@ -1,10 +1,15 @@
 package live.kill00.sample
 
+import dev.jorel.commandapi.CommandAPI
+import dev.jorel.commandapi.CommandAPIBukkitConfig
 import dev.jorel.commandapi.CommandAPICommand
 import dev.jorel.commandapi.executors.PlayerCommandExecutor
 import org.bukkit.plugin.java.JavaPlugin
 
 class Main : JavaPlugin() {
+    override fun onLoad() {
+        CommandAPI.onLoad(CommandAPIBukkitConfig(this).silentLogs(true).verboseOutput(false))
+    }
     override fun onEnable() {
         logger.info("Working!")
 
